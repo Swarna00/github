2f01fca7af65de02194ed60850961340f70c2e69
Swarna Barua <121425868+Swarna00@users.noreply.github.com>
2022-12-28 19:30:58 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+(function(){
+       let screen = document.querySelector('.screen');
+       let button = document.querySelector('.btn');
+       let btn = document.querySelector('.btn outline');                  
+
+})();
